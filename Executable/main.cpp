7190722ee2d8a37a19e71c517dec7e1f6a97c50a
ThePhoenix93 <francescofinotto@hotmail.com>
2022-08-02 09:28:31 +0200

    first commit: create 2 project exe: launch application Lib: have oll the opengl code

---
+++ Executable/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Test.h"
+
+int main(int, char**) {
+    Test t{};
+}
---
+++ Library/src/Test.cpp
@@ -0,0 +1,12 @@
+#include "../include/Test.h"
+#include <iostream>
+
+Test::Test()
+{
+    std::cout<<"Library linked"<<std::endl;
+}
+
+Test::~Test()
+{
+
+}
